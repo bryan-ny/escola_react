6ff2350129e99181fca862b79eb8588374591324
mariav28 <cavichiolo.maria@escola.pr.gov.br>
2025-10-10 10:39:32 -0300

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Navbar() {
+export default function NavBar() {
     return (
-        <Nav className="flex justify-center gap-6 bg-gray-200 p-4 shadow-md">
+        <nav className="flex justify-center gap-6 bg-gray-200 p-4 shadow-md">
             <a href="#home" className="hover:text-blue-600">Home</a>
             <a href="#escola" className="hover:text-blue-600">Escola</a>
             <a href="#mim" className="hover:text-blue-600">SobreMim</a>
-        </Nav>
+        </nav>
     );
 }
