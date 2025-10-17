33b8e9cb2bec80295b3f61cbac84b0b4652cd96c
mariav28 <cavichiolo.maria@escola.pr.gov.br>
2025-10-17 10:34:12 -0300

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function footer(){
+  return(
+    <footer className="Footer">
+      <div className="Footer-bottom">Otalípio Pereira De Andrade</div> 
+    </footer>
+  );
+}
